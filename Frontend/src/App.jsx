c7e24624fd1d4164cfe71e1e9ed3b2903295a3c9
samindikaLakshan <samindikalakshan71@gmail.com>
2026-04-22 02:28:46 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -93,7 +93,7 @@ function App() {
     try {
       const endpoint = mode === "encrypt" ? "encrypt" : "decrypt";
 
-      const response = await axios.post(`http://127.0.0.1:8001/${endpoint}`, formData, {
+      const response = await axios.post(`https://shiftblock-backend-axamhwfxfkcffubf.eastasia-01.azurewebsites.net/${endpoint}`, formData, {
         responseType: 'blob', 
       });
 
